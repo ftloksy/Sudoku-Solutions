d09635168a2f5b62c0a1450cf9e1f62dea7a0c9f
Frankie Chow <freakchow@gmail.com>
2023-06-13 16:33:31 +0100

    Document OneToNineTester.py

--- OneToNineTester.py
+++ OneToNineTester.py
@@ -1,16 +1,25 @@
+# This is the Tester tools class.
+# It will need pass needTest <array> for testing.
+# It will do two tests, 
+# 
+# allItemIsBetweenOneToNine : this will check needTest
+# every items is between 1-9 ,
+#
+# allItemIsUniqueNumber : this test will check every 
+# items is unique. No two items has same number.
 class Tester:
   def __init__(self, needTest):
     self.thisResult = True
     self.needTest = needTest
-    self.allItemIsBetweenOneToNine()
-    self.allItemIsUniqueNumbers()
+    self.allItemsIsBetweenOneToNine()
+    self.allItemsIsUniqueNumbers()
 
-  def allItemIsBetweenOneToNine(self):
+  def allItemsIsBetweenOneToNine(self):
     for i in self.needTest:
       if i > 9 or i < 0:
         self.thisResult = False
   
-  def allItemIsUniqueNumbers(self):
+  def allItemsIsUniqueNumbers(self):
     for i in self.needTest:
       if i != 0:
         hasMatch = 0
